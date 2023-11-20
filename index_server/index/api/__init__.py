1b4da820de02b786e4c8c7426e8ce180015a3c3c
laisnajjar <laisnajj@umich.edu>
2023-11-20 13:38:01 -0500

    config config i miss u

--- index_server/index/api/__init__.py
+++ index_server/index/api/__init__.py
@@ -1,10 +1,3 @@
-"""Index API module."""
-import os
-from flask import Flask
-import index.api.main
+"""API for index server."""
 
-
-app = Flask(__name__)
-
-app.config["INDEX_PATH"] = os.getenv("INDEX_PATH", "inverted_index_1.txt")
-index.api.main.load_index(app.config["INDEX_PATH"])
+from index.api.main import app
--- index_server/index/api/main.py
+++ index_server/index/api/main.py
@@ -1,7 +1,8 @@
 """Main API file for the index server."""
+from pathlib import Path
+import os
 import flask
 from flask import request, jsonify, json
-
 
 app = flask.Flask(__name__)
 
@@ -12,19 +13,25 @@ def load_index(path):
     pagerank_file = {}
     stopwords = set()
 
+    # Get the paths
+    cwd = Path.cwd()
+    inverted_file_path = os.path.join(cwd, "index_server/index/inverted_index", path)
+    pagerank_file_path = os.path.join(cwd, "index_server/index/", "pagerank.out")
+    stopwords_file_path = os.path.join(cwd, "index_server/index/", "stopwords.txt")
     # Load inverted index
-    with open(path, 'r', encoding='utf-8') as file:
+    with open(inverted_file_path, 'r', encoding='utf-8') as file:
         for line in file:
-            word = line[0]
-            values = line[1:]
+            parts = line.split()
+            word = parts[0]
+            values = parts[1:]
             inverted_index_file[word] = values
-    # Load pagerank
-    with open('pagerank.out', 'r', encoding='utf-8') as file:
+    # # Load pagerank
+    with open(pagerank_file_path, 'r', encoding='utf-8') as file:
         for line in file:
             doc_id, score = line.split(',')
             pagerank_file[doc_id] = score
-    # Load stopwords
-    with open('stopwords.txt', 'r', encoding='utf-8') as file:
+    # # Load stopwords
+    with open(stopwords_file_path, 'r', encoding='utf-8') as file:
         stopwords = set(file.read().split('\n'))
 
     return inverted_index_file, pagerank_file, stopwords
@@ -36,6 +43,12 @@ def index():
         "hits": "/api/v1/hits",
         "url": "/api/v1/"
     })
+
+@app.route('/api/v1/hits', methods=['GET'])
+def hits():
+    """Return the hits for a given query."""
+    query = request.args.get('q', default='', type=str)
+    weight = request.args.get('w', default=0.5, type=float)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9000)
